8a286cbd20d21cf0a2956d2d882635e15c92ba28
Moorindal <moorindal@ymail.com>
2019-02-20 11:01:06 -0500

    Improved markup, added skills info

--- src/app/scroll-button/scroll-button.component.ts
+++ src/app/scroll-button/scroll-button.component.ts
@@ -1,10 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ScrollService } from 'app/scroll.service';
+import { trigger, state, style, animate, transition, group } from '@angular/animations';
 
 @Component({
   selector: 'app-scroll-button',
   templateUrl: './scroll-button.component.html',
-  styleUrls: ['./scroll-button.component.scss']
+  styleUrls: ['./scroll-button.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate('500ms ease-in', style({opacity: 1}))
+      ]),
+      transition(':leave', [
+        animate('500ms ease-in', style({opacity: 0}))
+      ])
+    ])
+  ]
 })
 export class ScrollButtonComponent implements OnInit {
   @Input() targetPane: string;
