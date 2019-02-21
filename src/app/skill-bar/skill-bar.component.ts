c98ce60803c4a38223bd88ba46bcae6784664839
Moorindal <moorindal@ymail.com>
2019-02-21 11:06:41 -0500

    Added a better animation for skill bars

--- src/app/skill-bar/skill-bar.component.ts
+++ src/app/skill-bar/skill-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { trigger, state, style, animate, transition, group } from '@angular/animations';
+import { trigger, state, style, animate, transition, group, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-skill-bar',
@@ -9,14 +9,15 @@ import { trigger, state, style, animate, transition, group } from '@angular/anim
     trigger('widen', [
       transition(':enter', [
         style({width: 0}),
-        animate('500ms ease-in', style({width: '100%'}))
-      ])
+          animate("0.5s {{delay}}ms ease-in", style({width: "{{percentage}}%"}))
+      ], {params : { percentage: "50", delay: 0 }})
     ])
   ]
 })
 export class SkillBarComponent implements OnInit {
   @Input() skill: string;
   @Input() percentage: number = 0;
+  @Input() delay: number = 0;
 
   constructor() { }
 
--- src/app/skills-pane/skills-pane.component.ts
+++ src/app/skills-pane/skills-pane.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills-pane.component.scss']
 })
 export class SkillsPaneComponent implements OnInit {
+  skillset: object[] = [
+    {name: "JavaScript", proficiency: 75},
+    {name: "Angular", proficiency: 60},
+    {name: "React", proficiency: 50},
+    {name: "Vue", proficiency: 25},
+    {name: "Typescript", proficiency: 40},
+    {name: "NodeJS", proficiency: 25},
+    {name: "PHP", proficiency: 70},
+    {name: "Rails", proficiency: 35},
+    {name: "MongoDB", proficiency: 60},
+    {name: "MySQL", proficiency: 45},
+    {name: "Unit Testing", proficiency: 35}
+  ];
 
   constructor() { }
 
