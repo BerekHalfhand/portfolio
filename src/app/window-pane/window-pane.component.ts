a9de1342ed4bbcb76caeb9d1b6566ed1cb03d279
Moorindal <moorindal@ymail.com>
2019-02-23 21:42:41 -0500

    Better photo arrangement

--- src/app/window-pane/window-pane.component.ts
+++ src/app/window-pane/window-pane.component.ts
@@ -10,13 +10,13 @@ import { ScrollService } from 'app/scroll.service';
     trigger('one', [
       transition(':enter', [
         style({transform: 'translateY(50%) translateX(400%) scale(1.5)'}),
-        animate('500ms 300ms ease-in', style({transform: 'translateX(-60%) translateY(-25%) rotate(5deg) scale(1)'}))
+        animate('500ms 300ms ease-in', style({transform: 'translateX(-35%) translateY(-20%) rotate(5deg) scale(1)'}))
       ])
     ]),
     trigger('two', [
       transition(':enter', [
         style({transform: 'translateY(25%) translateX(400%) scale(1.5)'}),
-        animate('500ms 500ms ease-in', style({transform: 'translateX(20%) translateY(5%) rotate(-5deg) scale(1)'}))
+        animate('500ms 500ms ease-in', style({transform: 'translateX(35%) translateY(20%) rotate(-5deg) scale(1)'}))
       ])
     ]),
     trigger('textbox', [
